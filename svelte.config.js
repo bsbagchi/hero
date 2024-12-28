b9dd503fce0e0838bc9ec9c9f6fa60a20b8b51f0
bsbagchi <brotisunder9@gmail.com>
2024-12-28 16:46:23 +0530

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
+	// Preprocessor for Vite
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// Replace adapter-auto with Cloudflare adapter
+		adapter: adapter(),
+
+		// You can add additional configurations here, if necessary
 	}
 };
 
